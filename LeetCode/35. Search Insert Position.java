c8a31129e4d951e609659de4604baf88aea170f6
Ajay Malakiya <ajaymakaliya55@gmail.com>
2023-02-22 17:02:42 +0530

    35. Search Insert Position

---
+++ LeetCode/35. Search Insert Position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        int mid;
+        while (l <= h) {
+            mid = (l + h) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                h = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+}
